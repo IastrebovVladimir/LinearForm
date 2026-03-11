3421a8bd1dedf245db5192975251f50bd118fcda
IastrebovVladimir <159059876+IastrebovVladimir@users.noreply.github.com>
2026-03-11 15:24:32 +0300

    Update linearform.h

--- linearform.h
+++ linearform.h
@@ -19,6 +19,7 @@ void setcoeff(LinearForm *line_form, size_t i, void *new_coeff);
 LinearForm *addition(const LinearForm* line_form1, const LinearForm* line_form2); // сделать отдельное кольцо на тесты 2+3 a+b
 LinearForm *subtraction(const LinearForm* line_form1, const LinearForm* line_form2);
 void scalar(LinearForm* line_form, void* factor);
-void valueForm(const LinearForm* line_form, const void* args, size_t n_args, void* result); // операции с указателями, переложить на main.c
+void evaluateForm(const LinearForm* line_form, const void* args, size_t n_args, void* result); // операции с указателями, переложить на main.c
 // сделать 2-ю реализацию
+
 #endif
