39dcee33b9933adbe7ef2f4b77a8bf97d6f20ba0
IastrebovVladimir <159059876+IastrebovVladimir@users.noreply.github.com>
2026-03-11 17:20:40 +0300

    Update menu.h

--- menu.h
+++ menu.h
@@ -9,9 +9,10 @@ void case_setcoeff(LinearForm* line_form, const TypeInfo *t);
 void case_show_coeffs(LinearForm *line_form);
 void case_addition(LinearForm **line_form, const TypeInfo *t, int *n);
 void case_substraction(LinearForm **line_form, const TypeInfo *t, int *n);
-void case_scalar(LinearForm* line_form, const TypeInfo *t);
+void case_scale_form(LinearForm* line_form, const TypeInfo *t);
 void case_evaluate_form(LinearForm *line_form, const TypeInfo *t);
 void case_operations(LinearForm* line_form);
 
 
 #endif
+
